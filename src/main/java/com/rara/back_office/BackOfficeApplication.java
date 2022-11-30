adf748ac8e44c59a4dec41d66c3af7bde6797ead
rara-dev <kksr1219@gmail.com>
2022-12-01 02:14:37 +0900

    Projcet 생성

---
+++ src/main/java/com/rara/back_office/BackOfficeApplication.java
@@ -0,0 +1,15 @@
+package com.rara.back_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackOfficeApplication.class, args);
+	}
+
+}
