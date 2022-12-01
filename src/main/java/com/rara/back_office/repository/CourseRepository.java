95553ec12e70465d2ae973287eff61443a693008
rara-dev <kksr1219@gmail.com>
2022-12-01 20:14:18 +0900

    @Repository 추가

--- src/main/java/com/rara/back_office/repository/CourseRepository.java
+++ src/main/java/com/rara/back_office/repository/CourseRepository.java
@@ -3,7 +3,9 @@ package com.rara.back_office.repository;
 import com.rara.back_office.entity.Course;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 	Optional<Course> findById(Long id);
 }
